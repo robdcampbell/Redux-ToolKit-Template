8ecc7a1a11b012d1bc84d8b35c3fd7e329be100a
robdcampbell <rdeancampbell@gmail.com>
2021-12-22 16:24:16 -0500

    Continued Notes, Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+/////// OLD /////
+
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
